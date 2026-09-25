1294ae2e6e46362216c5e19a014a3e1284779137
Dmytry-S <sviridoffd@gmail.com>
2026-09-25 14:14:44 +0300

    Add tests

---
+++ tests/sortable-table.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('TC-001, Checkbox is selected', async ({ page }) => {
+    await page.goto('/laboratory/interactions');
+    await page.locator(`//input[@aria-label="Вибрати Авторизація"]`).click();
+    await expect(page.locator(`//span[@data-testid="interactions-selected-count"]`)).toContainText('1');
+});
+
+test('TC-002, Sorting is correct', async ({ page }) => {
+    await page.goto('/laboratory/interactions');
+    await page.locator('//button[@data-testid="interactions-sort-name"]').click();
+    await expect(page.locator(`//tbody/tr[1]/td[2]`)).toHaveText('Створення статті');
+});
