444cb60b2ba7d13d46498205ccdcb87951e8d0ca
Dmytry-S <sviridoffd@gmail.com>
2026-09-25 14:20:57 +0300

    Update tests

--- tests/sortable-table.spec.ts
+++ tests/sortable-table.spec.ts
@@ -4,10 +4,14 @@ test('TC-001, Checkbox is selected', async ({ page }) => {
     await page.goto('/laboratory/interactions');
     await page.locator(`//input[@aria-label="Вибрати Авторизація"]`).click();
     await expect(page.locator(`//span[@data-testid="interactions-selected-count"]`)).toContainText('1');
+    await page.locator(`//input[@aria-label="Вибрати Завантаження файлу"]`).click();
+    await expect(page.locator(`//span[@data-testid="interactions-selected-count"]`)).toContainText('2');
 });
 
 test('TC-002, Sorting is correct', async ({ page }) => {
     await page.goto('/laboratory/interactions');
     await page.locator('//button[@data-testid="interactions-sort-name"]').click();
     await expect(page.locator(`//tbody/tr[1]/td[2]`)).toHaveText('Створення статті');
+    await page.locator('//button[@data-testid="interactions-sort-name"]').click();
+    await expect(page.locator(`//tbody/tr[1]/td[2]`)).toHaveText('Авторизація');
 });
